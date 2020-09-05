bdb5d78aa8e8106fc8123a1ceba0e7a999a02ee2
RahifM <faizel326@gmail.com>
2020-09-05 14:51:00 +0000

    Add wt88047 (Redmi 2)
    Signed-off-by: RahifM <faizel326@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-devices=('blueline' 'bonito' 'coral' 'crosshatch' 'emulator' 'lake' 'marlin' 'mido' 'potter' 'river' 'taimen')
+devices=('blueline' 'bonito' 'coral' 'crosshatch' 'emulator' 'lake' 'marlin' 'mido' 'potter' 'river' 'taimen' 'wt88047')
 
 function lunch_devices() {
     add_lunch_combo du_${device}-user
